ccfc2e51437a07472ee6d755660c78b4a129e809
ShlokeBinani <shlokebinani@gmail.com>
2026-08-24 00:13:41 +0530

    Remove pino-http from app.ts to fix Vercel boot

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -9,25 +9,6 @@ import { logger } from "./lib/logger.js";
 
 const app: Express = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  }),
-);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
